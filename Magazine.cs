13cc6b60ecd83ec8812c2791ff5b6732821c19bd
sean Y <sean.youngs@outlook.com>
2023-01-30 20:51:07 +0000

    added initial magazine logic

---
+++ Magazine.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magazine : MonoBehaviour
+{
+
+    // mag insert pos Vector3(0,-0.03,-0.005)
+    // mag insert rot Vector3(9,0,0)
+        // quaternion Quaternion(0.0784591213,0,0,0.996917367)
+    // mag hold rot -30, 30, 0
+
+    private float     timer         = 0f;
+    private bool      ejectMag      = true;
+    private bool      insertMag     = false;
+    private Vector3   magInsertPos  = new Vector3(0f, -0.03f, -0.005f);
+    private Vector3   magInstertRot = new Vector3(9f, 0f, 0f);
+    private Vector3[] magHoldPos    = new[] { 
+        new Vector3(0f, -0.162f, -0.044f),
+        new Vector3(-0.19f, 0.069f, -0.007f)
+    };
+    private Vector3   magHoldRot    = new Vector3(-30, 30, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    public void ManipulateMag()
+    {
+        if(ejectMag)
+        {
+            // eject
+        }
+    }
+
+}
