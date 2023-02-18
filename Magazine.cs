889211b41b5c44daea5e48d1bb806d05efe8b2ee
sean Y <sean.youngs@outlook.com>
2023-02-18 22:03:36 +0000

    removed logic for lerping

--- Magazine.cs
+++ Magazine.cs
@@ -4,29 +4,13 @@ using UnityEngine;
 
 public class Magazine : MonoBehaviour
 {
-
-    // mag insert pos Vector3(0,-0.03,-0.005)
-    // mag insert rot Vector3(9,0,0)
-    // quaternion Quaternion(0.0784591213,0,0,0.996917367)
-    // mag hold rot -30, 30, 0
-
-    [SerializeField] int                magLerpSpeed = 5;
     [SerializeField] private AudioClip  magInsertAud, magEjectAud;
 
-    private float       timer         = 0f;
-    private float       timer2        = 0f;
     private bool        magInGun      = true;
     private bool        ejectMag      = false;
     private bool        insertMag     = false;
     private Animator    pistolAnim; //for accessing animator
     private AudioSource audioS;
-    private Vector3     magInsertPos  = new Vector3(0f, -0.03f, -0.005f);
-    private Vector3     magInstertRot = new Vector3(9f, 0f, 0f);
-    private Vector3[]   magHoldPos    = new[] { 
-        new Vector3(0f, -0.162f, -0.044f),
-        new Vector3(-0.19f, 0.069f, -0.007f)
-    };
-    private Vector3     magHoldRot    = new Vector3(-30, 30, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -83,13 +67,4 @@ public class Magazine : MonoBehaviour
             magInGun = true;
         }
     }
-
-    // ---------------------------------------
-    // COROUTINES
-
-    IEnumerator LerpMag(string point)
-    {
-        yield return null;
-    }
-
 }
